ba152bd174239241028c71ea49d9781e448b04b0
Mawuena Melomey <mawuenamelomey@gmail.com>
2014-09-07 15:13:04 +0000

    third commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,27 @@
-'use strict';
+//'use strict';
 
 /* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+        {'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.'}
+    ];
+    $scope.name = "World";
+});
+
+//describe('PhoneListCtrl', function(){
+//
+//    it('should create "phones" model with 3 phones', function() {
+//        var scope = {},
+//            ctrl = new PhoneListCtrl(scope);
+//
+//        expect(scope.phones.length).toBe(3);
+//    });
+//
+//});
